dd2a929110759a96e58b02f34107e90054e00ca2
gunjaashok1122 <gunjaashok1122@gmail.com>
2026-06-01 09:32:29 +0530

    Remove @rork-ai/toolkit-sdk dependency and rename bundle/scheme branding references

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -1,12 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-let config = getDefaultConfig(__dirname);
-
-try {
-  const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
-  config = withRorkMetro(config);
-} catch (e) {
-  // Rork SDK metro wrapper failed or not present, fallback to default
-}
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
